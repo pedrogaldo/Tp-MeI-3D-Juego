e00297ef872e8fa60106239f51bfa8b7edcd7aa6
pedrogaldo <galdo.pedro13@gmail.com>
2022-06-01 12:12:56 -0300

    Initial commit

---
+++ Assets/movimiento.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movimiento : MonoBehaviour
+{
+    public float speed;
+    public float rotationSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+        if (Input.GetKey(KeyCode.W))
+        {
+            for (speed = 0; speed <= 0.04; speed += 0.001f)
+            {
+                transform.Translate(0, 0, speed);
+            }
+        }
+
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.eulerAngles -= new Vector3(0, rotationSpeed, 0);
+        }
+
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.eulerAngles += new Vector3(0, rotationSpeed, 0);
+        }
+           
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Tp-MeI-3D-Juego
+ 

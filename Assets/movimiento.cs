2ecde9622b8965b9c00478ca04c95d0ee35afc83
pedrogaldo <101400428+pedrogaldo@users.noreply.github.com>
2022-06-02 09:20:20 -0300

    Aceleracion semi-terminada
    aceleracion del auto semi terminada

--- Assets/movimiento.cs
+++ Assets/movimiento.cs
@@ -4,38 +4,39 @@ using UnityEngine;
 
 public class movimiento : MonoBehaviour
 {
+ 
     public float speed;
     public float rotationSpeed;
+    public float maxSpeed;
+    public float acceleration;
+    
 
     // Start is called before the first frame update
     void Start()
     {
-        
+ 
     }
 
     // Update is called once per frame
     void Update()
     {
-       
+
         if (Input.GetKey(KeyCode.W))
         {
-            for(speed=0; speed<0.01; speed+=0.0001f)
+            transform.Translate(0, 0, speed * Time.deltaTime);
+
+            //aceleracion del auto
+            if (speed < maxSpeed)
             {
                 transform.Translate(0, 0, speed);
+                speed += 0.01f;
             }
         }
 
 
-        if (Input.GetKeyUp(KeyCode.W))
-        {
-            while (speed > 0)
-            {
-                speed -= 0.0001f;
-            }
-        }
 
         if (speed > 0)
-        { 
+        {
             if (Input.GetKey(KeyCode.A))
             {
                 transform.eulerAngles -= new Vector3(0, rotationSpeed, 0);
@@ -52,11 +53,12 @@ public class movimiento : MonoBehaviour
         if (speed == 0)
         {
             if (Input.GetKey(KeyCode.S))
-            { 
+            {
                 transform.Translate(0, 0, -speed);
             }
         }
-           
 
+       
     }
+
 }

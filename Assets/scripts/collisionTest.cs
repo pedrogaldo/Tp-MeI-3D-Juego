e6e2a606df01bd5dfa1a16dab426f5fa0a43f47b
pedrogaldo <101400428+pedrogaldo@users.noreply.github.com>
2022-06-07 14:44:42 -0300

    Prueba de colisiones
    hice una prueba de colisiones para ganar

---
+++ Assets/scripts/collisionTest.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class collisionTest : MonoBehaviour
+{
+
+    void OnCollisionEnter(Collision col)
+    {
+
+        if (col.gameObject.name == "test_collision")
+        {
+            SceneManager.LoadScene("ganaste_test");
+        }
+
+    }
+
+}

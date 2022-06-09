765b244368d55e740206c0def96ca93077938324
pedrogaldo <101400428+pedrogaldo@users.noreply.github.com>
2022-06-09 15:10:08 -0300

    commit 13
    segui con sonidos, escenas etc...

---
+++ Assets/scripts/Perder.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Perder : MonoBehaviour
+{
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y==-1)
+        {
+            SceneManager.LoadScene("Perdiste");
+        }
+    }
+}

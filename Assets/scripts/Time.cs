01b61b840875582913c5de173c931cdc46a1e963
pedrogaldo <101400428+pedrogaldo@users.noreply.github.com>
2022-06-10 23:58:26 -0300

    circuito añadido

---
+++ Assets/scripts/Time.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Time : MonoBehaviour
+{
+    public GameObject timeDisplay;
+    public int tiempoRestante;
+    public bool restando = false;
+
+    void Start()
+    {
+        timeDisplay.GetComponent<Text>().text = "00:" + tiempoRestante;
+    }
+
+    void Update()
+    {
+        if (restando == false && tiempoRestante > 0)
+        {
+            StartCoroutine(TimerTake());
+        }
+
+        if (tiempoRestante == 0)
+        {
+            SceneManager.LoadScene("Perdiste");
+        }
+    }
+
+    IEnumerator TimerTake()
+    {
+        restando = true;
+        yield return new WaitForSeconds(1);
+        tiempoRestante -= 1;
+        timeDisplay.GetComponent<Text>().text = "00:" + tiempoRestante;
+        restando = false;
+
+    }
+}
